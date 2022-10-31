493c5b446657bde0a2b2802792b57e19f7cdce25
Atharvat <tagalpallewaratharva@gmail.com>
2022-10-31 23:54:37 +0530

    onedrive

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ import cookieParser from "cookie-parser";
 
 import connectDatabase from "./services/connectDB.js";
 connectDatabase();
+import onedriveRoutes from "./routes/onedrive/onedrive.routes.js";
+import coursesRoutes from "./routes/courses.routes.js";
+import fs from "fs";
 
 const app = express();
 const PORT = config.port;
@@ -19,6 +22,33 @@ app.use(cookieParser());
 app.use(cors());
 app.use("/", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/onedrive/", onedriveRoutes);
+app.use("/", coursesRoutes);
+
+app.use(
+	"/homepage",
+	catchAsync(async (req, res, next) => {
+		let access_token;
+		access_token = fs.readFileSync("./access-token.token", "utf-8");
+
+		var config = {
+			method: "get",
+			url: "https://graph.microsoft.com/v1.0/me",
+			headers: {
+				Authorization: `Bearer ${access_token}`,
+			},
+		};
+
+		const response = await axios.get(config.url, {
+			headers: config.headers,
+		});
+		// console.log(response);
+
+		if (!response.data) throw new AppError(401, "Access Denied");
+
+		res.json(response.data);
+	})
+);
 
 // Error handler
 app.use((err, req, res, next) => {
