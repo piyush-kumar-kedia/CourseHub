8b2d209a15fefba426bcbed3b526da413a1d4eae
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2023-02-12 03:14:29 +0530

    added mobile app

---
+++ mobile/lib/screens/login_screen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  Function callback;
+ LoginScreen({super.key,required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/login_page.png'), fit: BoxFit.cover),
+          ),
+        ),
+        Container(
+          alignment: Alignment.topRight,
+          child: SizedBox(
+            width: 120.0,
+            height: double.infinity,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 70, 20, 0),
+          alignment: Alignment.topRight,
+          child: RotatedBox(
+              quarterTurns: 1,
+              child: Text("CourseHub",
+                  style: TextStyle(
+                      fontFamily: "Proxima Nova",
+                      fontSize: 57.87,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white))),
+        ),
+        Column(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.topRight,
+                // margin: const EdgeInsets.fromLTRB(0, 482, 50, 0),
+                child: Text("by",
+                    style: TextStyle(
+                        fontFamily: "Proxima Nova",
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black))),
+
+            // Expanded(
+            //   child: FittedBox(
+            //     child: FlutterLogo(),
+            //   ),
+            // ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+                alignment: Alignment.topRight,
+                margin: const EdgeInsets.fromLTRB(0, 482, 50, 0),
+                child: Text("by",
+                    style: TextStyle(
+                        fontFamily: "Proxima Nova",
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white))),
+            Container(
+              margin: const EdgeInsets.fromLTRB(275, 0, 0, 0),
+              height: 90,
+              width: 70,
+              child: Image(image: AssetImage("assets/img.png")),
+            )
+          ],
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            height: 160,
+            width: double.infinity,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(color: Color(0xffFECF6F)),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            height: 140,
+            width: 302,
+            child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa odio nibh eu eu nulla ac vestibulum amet. Ultrices magna ",
+                style: TextStyle(
+                    fontFamily: "Proxima Nova",
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black)),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 750, 0, 0),
+          alignment: Alignment.bottomCenter,
+          child: Column(
+            children: [
+              Builder(builder: (context) {
+                return GestureDetector(
+                  onTap: () => {
+                   callback()
+                  },
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    width: 333,
+                    height: 55,
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Container(
+                              margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                              child: Image.asset('assets/image 4.png')),
+                          Text("       Sign in with Microsoft",
+                              style: TextStyle(
+                                  fontFamily: "Proxima Nova",
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.white),
+                                  ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
---
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_appauth
+import flutter_web_auth
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
+}
