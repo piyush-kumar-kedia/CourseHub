7a399811ce9cf3a2ab896d8977b6373651446a4d
ranjan_kuldeep <95350799+ranjankuldeep@users.noreply.github.com>
2023-02-13 18:09:49 +0530

    File upload UI added

--- client/src/screens/contributions/index.jsx
+++ client/src/screens/contributions/index.jsx
@@ -1,122 +1,99 @@
 import Wrapper from "./components/wrapper";
 import SectionC from "./components/sectionC";
 import axios from "axios";
-import SearchBar from "./components/searchbar";
+import { FilePond, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import { useRef } from "react";
+
 import "./styles.scss";
 
-import { useState } from "react";
 const Contributions = () => {
-    const [File_, setFile] = useState(null);
-    const [course, setCourse] = useState(null);
-
-    async function handleUpload() {
-        if (!File_) return;
-        const formData = new FormData();
-        formData.append("upload", File_);
-        try {
-            console.log(formData);
-            await axios.post("http://localhost:4002/upload", formData);
-        } catch (error) {
-            console.log(error);
-        }
+    let pond = useRef();
+    async function handleSubmit() {
+        await pond.processFiles();
+        const collection = document.getElementsByClassName("contri");
+        const contributionSection = collection[0];
+        contributionSection.classList.remove("show");
+        pond.removeFiles();
     }
-    const handleSetCourse = (courseData) => {
-        console.log(courseData);
-        setCourse(courseData);
-        return;
-    };
-
     return (
         <SectionC>
             <Wrapper>
                 <div className="head">Contribute to CourseHub</div>
-                {/* <form onSubmit={(e) => e.preventDefault()}> */}
-                <div
-                    className="course"
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                >
-                    <label htmlFor="course" className="label_course">
-                        COURSE CODE :
-                    </label>
-                    <SearchBar handleSetCourse={handleSetCourse} />
-                    {/* <input
+                <form>
+                    <div className="course">
+                        <label htmlFor="course" className="label_course">
+                            COURSE CODE :
+                        </label>
+                        <input
                             placeholder="Course Code"
                             name="course"
                             className="input_course"
-                        ></input> */}
-                </div>
-                <div className="section">
-                    <label htmlFor="section" className="label_section">
-                        SECTION :
-                    </label>
-                    <select name="section" className="select_section">
-                        <option value="Lecture Slides">Lecture Slides</option>
-                        <option value="Tutorials">Tutorials</option>
-                        <option value="Exams">Exams</option>
-                        <option value="Notes">Notes</option>
-                        <option value="Assignments">Assignments</option>
-                    </select>
-                </div>
-                <div className="year">
-                    <label htmlFor="year" className="label_year">
-                        YEAR :
-                    </label>
-                    <select name="year" className="select_year">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                stroke-linejoin="round"
-                                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                            />
-                        </svg>
-
-                        <option value="2022">2022</option>
-                        <option value="2019">2019</option>
-                        <option value="2018">2018</option>
-                        <option value="2017">2017</option>
-                    </select>
-                </div>
-                <div className="description">
-                    <label htmlFor="description" className="label_description">
-                        DESCRIPTION :
-                    </label>
-                    <textarea
-                        name="description"
-                        className="input_description"
-                        placeholder="Give a brief description"
-                    ></textarea>
-                </div>
-                {/* </form> */}
-                <div className="file">
-                    <label for="file_upload" className="label_file">
-                        <input
-                            type="file"
-                            id="file_upload"
-                            accept="application/pdf"
-                            name="upload"
-                            onChange={(e) => {
-                                setFile(e.target.files[0]);
-                            }}
-                        />
-                        <img src="../../../public/upload.png" height="37px" width="37px" />
-                        UPLOAD
-                    </label>
-                    <div className="uploaded">
-                        <span>UPLOADED:</span> folder/file
+                        ></input>
                     </div>
-                </div>
+                    <div className="section">
+                        <label htmlFor="section" className="label_section">
+                            SECTION :
+                        </label>
+                        <select name="section" className="select_section">
+                            <option value="Lecture Slides">Lecture Slides</option>
+                            <option value="Tutorials">Tutorials</option>
+                            <option value="Exams">Exams</option>
+                            <option value="Notes">Notes</option>
+                            <option value="Assignments">Assignments</option>
+                        </select>
+                    </div>
+                    <div className="year">
+                        <label htmlFor="year" className="label_year">
+                            YEAR :
+                        </label>
+                        <select name="year" className="select_year">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                                />
+                            </svg>
 
-                <div className="button" onClick={handleUpload}>
+                            <option value="2022">2022</option>
+                            <option value="2019">2019</option>
+                            <option value="2018">2018</option>
+                            <option value="2017">2017</option>
+                        </select>
+                    </div>
+                    <div className="description">
+                        <label htmlFor="description" className="label_description">
+                            DESCRIPTION :
+                        </label>
+                        <textarea
+                            name="description"
+                            className="input_description"
+                            placeholder="Give a brief description"
+                        ></textarea>
+                    </div>
+                </form>
+
+                <FilePond
+                    allowMultiple={true}
+                    maxFiles={40}
+                    server={"http://localhost:8080/onedrive/upload"}
+                    instantUpload={false}
+                    ref={(ref) => {
+                        pond = ref;
+                    }}
+                />
+                <div className="uploaded">
+                    <span>UPLOADED:</span> folder/file
+                </div>
+                <div className="button" onClick={handleSubmit}>
                     SUBMIT
                 </div>
             </Wrapper>
