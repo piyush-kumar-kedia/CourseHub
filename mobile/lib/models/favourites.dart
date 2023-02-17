f13dac7d049f8a3dc7c229af0debb19ac230c8f7
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2023-02-17 20:10:13 +0530

    login apis

---
+++ mobile/lib/models/favourites.dart
@@ -0,0 +1,13 @@
+class Favourites {
+  String name;
+  String id;
+  String path;
+  String code;
+
+  Favourites({
+    required this.code,
+    required this.id,
+    required this.name,
+    required this.path
+  })
+}
---
+++ mobile/lib/models/user.dart
@@ -0,0 +1,14 @@
+import 'package:test1/models/favourites.dart';
+
+class User {
+  String name;
+  String email;
+  int rollNumber;
+  int semester;
+  String degree;
+  List<Courses> courses;
+  String? department;
+  List<Favourites> favourites;
+
+  
+}
