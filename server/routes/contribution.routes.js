984f711adbaabc4725d4c0aec49a51b1cca1fc5f
Ankit D <ankit.99d@gmail.com>
2023-02-19 23:42:30 +0530

    added mobile uploads

--- server/routes/contribution.routes.js
+++ server/routes/contribution.routes.js
@@ -3,11 +3,18 @@ const router = express.Router();
 import ContributionController from "../controllers/contribution.controller.js";
 import catchAsync from "../utils/catchAsync.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import multer from "multer";
 
+export const upload = multer({ dest: "external/mobile" });
 router.get("/", isAuthenticated, ContributionController.GetMyContributions);
 router.get("/all", ContributionController.GetAllContributions);
 router.post("/", catchAsync(ContributionController.CreateNewContribution));
 router.post("/upload", catchAsync(ContributionController.HandleFileUpload));
+router.post(
+    "/upload/mobile",
+    upload.array("files"),
+    catchAsync(ContributionController.MobileFileUploadHandler)
+);
 // router.get("/:id", catchAsync(ContributionController.CreateNewContribution));
 
 export default router;
