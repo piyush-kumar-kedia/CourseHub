d1056a69e7081abe028ecc14819fa5e2fe08d713
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2023-02-21 19:20:57 +0530

    added script for deleting a course

---
+++ server/scripts/deleteSpecificCourse.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import CourseModel from "../models/course.model.js";
+import { FileModel, FolderModel } from "../models/course.model.js";
+import SearchResults from "../models/search.model.js";
+const mongoUri = `mongodb+srv://admin:wlNISeAMiWd3JPPR@coursehub-dev.wyyvbkk.mongodb.net/?retryWrites=true&w=majority`;
+
+async function safeDeleteCourse(code, name) {
+    let sanitizedCode = code.toLowerCase();
+    let sanitizedName = name.toLowerCase();
+
+    try {
+        await CourseModel.deleteOne({ code: sanitizedCode });
+    } catch (error) {
+        console.log("Error Deleting Course");
+    }
+    try {
+        await FileModel.deleteMany({
+            course: `${sanitizedCode} - ${sanitizedName}`,
+        });
+    } catch (error) {
+        console.log("Error Deleting Files");
+    }
+    try {
+        await FolderModel.deleteMany({
+            course: sanitizedCode,
+        });
+    } catch (error) {
+        console.log("Error Deleting Folders");
+    }
+    try {
+        await SearchResults.findOneAndUpdate({ code: sanitizedCode }, { isAvailable: false });
+    } catch (error) {
+        console.log("Error Updating search results");
+    }
+    console.log("Successfully delete course", sanitizedCode, sanitizedName);
+    console.log("Exiting...");
+    // process.exit(0);
+}
+mongoose.set("strictQuery", false);
+
+//TODO: ADD course code and name here
+mongoose
+    .connect(mongoUri)
+    .then(() => {
+        safeDeleteCourse("ce101", "engineering design");
+    })
+    .catch((e) => console.log(e));
