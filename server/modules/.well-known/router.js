e3309ccb83f0d301513065bc5246be175147074f
Priyanshu Srivastava <97830682+GeekyPS@users.noreply.github.com>
2023-04-13 13:08:59 +0530

    Update router.js

--- server/modules/.well-known/router.js
+++ server/modules/.well-known/router.js
@@ -8,7 +8,7 @@ router.get("/assetlinks.json", async (req, res, next) => {
             relation: ["delegate_permission/common.handle_all_urls"],
             target: {
                 namespace: "CourseHub",
-                package_name: "com.codingclubiitg.coursehub",
+                package_name: "com.codingclub.coursehub",
                 sha256_cert_fingerprints: [
                     "27:C0:74:E7:D7:78:08:6C:B8:31:F3:05:48:4E:9B:A1:EA:6A:7E:9D:37:29:4E:07:D6:78:7A:4D:B8:0D:AE:D3",
                 ],
