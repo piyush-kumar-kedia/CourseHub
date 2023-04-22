66b95b159c160ec5470f0dd200d87b527e071229
Ankit D <ankit.99d@gmail.com>
2023-04-22 23:14:54 +0530

    fixed login (1/2)

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/file", onedriveRoutes);
