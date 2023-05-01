3884bb171828df4b42055b94939b8cc5c60bff76
Ankit D <ankit.99d@gmail.com>
2023-05-01 22:15:17 +0530

    timetable fix

--- server/modules/timetable/timetable.routes.js
+++ server/modules/timetable/timetable.routes.js
@@ -69,13 +69,13 @@ router.get(
             // console.log(course);
             if (all) {
                 // toReturn[course.code] = all;
-                toReturn.push({ ...all._doc, name: course.name });
+                toReturn.push({ ...all._doc, name: course.name, found: true });
             } else if (individual) {
                 // toReturn[course.code] = individual;
-                toReturn.push({ ...individual._doc, name: course.name });
+                toReturn.push({ ...individual._doc, name: course.name, found: true });
             } else if (fromTo) {
                 // toReturn[course.code] = fromTo;
-                toReturn.push({ ...fromTo._doc, name: course.name });
+                toReturn.push({ ...fromTo._doc, name: course.name, found: true });
             } else {
                 // toReturn[course.code] = { notFound: true };
                 toReturn.push({ code: course.code, found: false, name: course.name });
