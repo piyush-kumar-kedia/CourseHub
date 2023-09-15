fa5c1d199bb63339ee2f6e36814b77906e8304bd
Ankit D <ankit.99d@gmail.com>
2023-09-15 15:02:14 +0530

    refresh courses for all

--- server/config/default.js
+++ server/config/default.js
@@ -8,5 +8,5 @@ export default {
     jwtSecret: process.env.JWT_SECRET,
     aesKey: process.env.AESKEY,
     adminJwtSecret: "32rytfhgv456ryt43ertfgy45rtfhh",
-    serverVersion: 1.2
+    serverVersion: 1.3
 };
