9f37fdf7f34936b1b7b478a9710a8cf6256904ec
Ankit D <ankit.99d@gmail.com>
2023-10-30 10:28:55 +0530

    fixed bugs, added admin panel dist since having build problems on vm

--- server/config/default.js
+++ server/config/default.js
@@ -3,10 +3,11 @@ import "dotenv/config";
 export default {
     port: process.env.PORT || 8080,
     mongoURI: process.env.MONGO_URI,
-    clientURL: process.env.CLIENT_URL,
+    // clientURL: process.env.CLIENT_URL,
+    clientURL: "https://www.coursehubiitg.in",
     mobileURL: process.env.MOBILE_URL,
     jwtSecret: process.env.JWT_SECRET,
     aesKey: process.env.AESKEY,
     adminJwtSecret: "32rytfhgv456ryt43ertfgy45rtfhh",
-    serverVersion: 1.3
+    serverVersion: 1.3,
 };
