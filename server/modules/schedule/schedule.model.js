a5bcd0ae0ad0154636b7af70d6b6a253d7e49b71
MjStar <starmj24@gmail.com>
2024-01-09 21:50:46 +0530

    .

--- server/modules/schedule/schedule.model.js
+++ server/modules/schedule/schedule.model.js
@@ -1,17 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import CourseModel from "../course/course.model.js";
+import User from "../user/user.model.js";
+
+const classDetails=mongoose.Schema({
+    startDateTime:{type:Date},
+    endDateTime:{type:Date},
+    location:{type:String},
+    professor:{type:String}
+},{_id:false})
+
 
 const ScheduleSchema=mongoose.Schema({
-    courseName:{type:String},
-    startTime:{type:Date},
-    endTime:{type:Date},
-    classLocation:{type:String},
-    branch:{type:String},
-    semester:{type:String},
+    course:{type:Schema.Types.ObjectId,ref:CourseModel},
+    classDetails,
+    datesCancelled:{type:[Date]},
+    classAdded:{type:[classDetails]},
+    notificationSubscribed:[{type:Schema.Types.ObjectId,ref:User}],
     day:{
         type:String,
         enum:['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
     }
 })
+
+
+
 
 const Schedule=mongoose.model('Schedule',ScheduleSchema);
 export default Schedule;
