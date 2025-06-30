ca158db40ad21aacb79c68090bec6bb8bb77314e
shreya-11733 <shreya.agrawal@iitg.ac.in>
2025-06-30 19:37:31 +0530

    for obtaining current academic session and year

--- server/config/academic.js
+++ server/config/academic.js
@@ -4,9 +4,9 @@ const currentYear = currdate.getFullYear().toString();
 const currentMonth = currdate.getMonth();
 
 var session;
-session='Jan-May';
-// if(currentMonth<=5||(currentMonth==6&&currdatenumber<=23)) session='Jan-May';
-// else session='Jul-Nov';
+//session='Jan-May';
+if(currentMonth<=5||(currentMonth==6&&currdatenumber<=23)) session='Jan-May';
+else session='Jul-Nov';
 
 export default {
     currentYear,
