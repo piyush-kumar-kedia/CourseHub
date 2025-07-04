3da87bbd5511912ae5833ad5199304395cd27c9e
DreamBot706 <187842058+DreamBot706@users.noreply.github.com.>
2025-07-04 16:28:28 +0000

    add logic to fetch coursedata upon initial load

--- client/src/api/Course.js
+++ client/src/api/Course.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 import root from "./server";
+
 export const getCourse = async (code) => {
     const resp = await axios.get(`${root}/api/course/${code}`);
     return resp;
 };
+
+export const getUserCourses = async (courses) => {
+    try{        
+        const resp = await axios.get(`${root}/api/course/getUserCourses`, {
+            params: {
+                courses: courses,
+            }
+        });
+        return resp.data;
+    }
+    catch(err){
+        console.log("Error:", err.message);
+    }
+}
--- server/modules/course/course.routes.js
+++ server/modules/course/course.routes.js
@@ -4,6 +4,7 @@ import {
     getAllCourses,
     getCourse,
     isCourseUpdated,
+    getUserCourses,
 } from "./course.controller.js";
 const router = express.Router();
 
@@ -11,8 +12,9 @@ import catchAsync from "../../utils/catchAsync.js";
 import isAuthenticated from "../../middleware/isAuthenticated.js";
 
 router.get("/", catchAsync(getAllCourses));
-router.get("/:code", catchAsync(getCourse));
+router.get("/getUserCourses", getUserCourses);
 router.get("/delete/:code", catchAsync(deleteCourseByCode));
 router.post("/isUpdated", isAuthenticated, catchAsync(isCourseUpdated));
+router.get("/:code", catchAsync(getCourse));
 
 export default router;
