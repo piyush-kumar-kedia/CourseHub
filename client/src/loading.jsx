161b983705d83ea132f869113e49ef60a79f0a9d
aditya-samal <samaladitya2004@gmail.com>
2025-08-07 00:21:22 +0530

    Fixed Sidebar Alignment

--- client/src/loading.jsx
+++ client/src/loading.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { getUser } from "./api/User";
 import { fetchUserCoursesData } from "./api/Course";
 import { toast } from "react-toastify";
-import "./loading.css";
 import { LoginUser } from "./actions/user_actions";
 import { useDispatch } from "react-redux";
 
@@ -43,13 +42,83 @@ const LoadingPage = () => {
     }, [dispatch, navigate]);
 
     return (
-        <div className="loading">
-            <div className="loader">
-                <div className="justify-content-center jimu-primary-loading"></div>
+        <div className="flex flex-col items-center justify-center h-screen w-screen bg-black text-white">
+            <div className="flex justify-center items-center relative">
+                <div className="jimu-primary-loading"></div>
             </div>
-            <div className="text">
+            <div className="mt-10 text-center">
                 <h2>Fetching your courses</h2>
             </div>
+
+            {/* Custom CSS for the loader animation */}
+            <style jsx>{`
+                .jimu-primary-loading:before,
+                .jimu-primary-loading:after {
+                    position: absolute;
+                    top: 0;
+                    content: "";
+                }
+
+                .jimu-primary-loading:before {
+                    left: -19.992px;
+                }
+
+                .jimu-primary-loading:after {
+                    left: 19.992px;
+                    -webkit-animation-delay: 0.32s !important;
+                    animation-delay: 0.32s !important;
+                }
+
+                .jimu-primary-loading:before,
+                .jimu-primary-loading:after,
+                .jimu-primary-loading {
+                    background: #ffffff;
+                    -webkit-animation: loading-keys-app-loading 0.8s infinite ease-in-out;
+                    animation: loading-keys-app-loading 0.8s infinite ease-in-out;
+                    width: 13.6px;
+                    height: 32px;
+                }
+
+                .jimu-primary-loading {
+                    text-indent: -9999em;
+                    margin: auto;
+                    position: relative;
+                    right: 0;
+                    top: 0;
+                    -webkit-animation-delay: 0.16s !important;
+                    animation-delay: 0.16s !important;
+                }
+
+                @-webkit-keyframes loading-keys-app-loading {
+                    0%,
+                    80%,
+                    100% {
+                        opacity: 0.75;
+                        box-shadow: 0 0 #3a3a3a;
+                        height: 32px;
+                    }
+                    40% {
+                        opacity: 1;
+                        box-shadow: 0 -8px #9e9e9e;
+                        height: 40px;
+                    }
+                }
+
+                @keyframes loading-keys-app-loading {
+                    0%,
+                    80%,
+                    100% {
+                        opacity: 0.75;
+                        box-shadow: 0 0 #c4c4c5;
+                        height: 32px;
+                    }
+                    40% {
+                        opacity: 1;
+                        box-shadow: 0 -8px #717171;
+                        height: 40px;
+                    }
+                }
+            `}</style>
         </div>
     );
 };
