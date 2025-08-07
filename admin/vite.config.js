7befd80aa0258ae0f67865c0eef1c53549475f6f
Sanjeebani Parida <sanjeebani14@gmail.com>
2025-08-08 00:30:25 +0530

    minor changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import root from "./src/server";
+import { API_BASE_URL } from "./src/apis";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/admin/',
+    base: "/admin/",
     plugins: [react(), tailwindcss()],
     server: {
         port: 5174,
         proxy: {
-            '/api': root,
+            "/api": API_BASE_URL,
         },
     },
 });
