01a3668c9aba67ba23d57c88b5fc3715a573d703
aditya-samal <samaladitya2004@gmail.com>
2025-08-07 13:21:22 +0530

    Test

--- client/src/api/Year.js
+++ client/src/api/Year.js
@@ -8,26 +8,26 @@ const API = axios.create({
     withCredentials: true,
 });
 
-// Attach token to every request if available (from localStorage)
+// Attach token to every request if available (from localStorage) wad
 API.interceptors.request.use((req) => {
     const user = JSON.parse(localStorage.getItem("profile"));
     if (user) req.headers.Authorization = `Bearer ${user.token}`;
     return req;
 });
 
-export const addYear = async ({ name, course}) => {
+export const addYear = async ({ name, course }) => {
     const { data } = await API.post("/year", {
         name,
         course,
-        childType:"Folder",
-        Children:[],
+        childType: "Folder",
+        Children: [],
     });
     return data;
 };
 
-export const deleteYear = async ({ folder, courseCode}) => {
+export const deleteYear = async ({ folder, courseCode }) => {
     const { data } = await API.delete("/year/delete", {
-        data:{folder, courseCode}
+        data: { folder, courseCode },
     });
     return data;
 };
