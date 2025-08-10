82c9fe81842c958e5b576c692f92833f769428e7
piyush-kumar-kedia <piyushkumarkedia02221@gmail.com>
2025-08-10 15:47:02 +0530

    fixed the mobile view of website

---
+++ client/src/mobile.jsx
@@ -0,0 +1,22 @@
+import "./mobile.css";
+
+const MobilePage = () => {
+    return (
+        <>
+            <div class="message">
+                <img
+                    src="https://img.icons8.com/fluency/96/000000/laptop.png"
+                    alt="Laptop"
+                    class="laptop-img"
+                />
+                <h1>CourseHub is only accessible through PC/Laptop</h1>
+                <p>
+                    We currently do not support mobile access. Please visit this site from a desktop
+                    or laptop device.
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default MobilePage;
